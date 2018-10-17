977d8e6f86d8b8b436dc519f6f5e4d7341e42eb9
Ramon Peart <ramon.peart@gmail.com>
2018-10-17 14:38:35 -0400

    Enabled the floatingActionButton to open new activity.

--- app/src/main/java/com/example/admin/mynotes/MainActivity.java
+++ app/src/main/java/com/example/admin/mynotes/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admin.mynotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,21 +12,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +42,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createNote(View view) {
+        intent = new Intent(this, CreateNote.class);
+        startActivity(intent);
     }
 }
